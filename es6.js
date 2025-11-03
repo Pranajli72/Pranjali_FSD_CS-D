fff151464d5186a05a91e9dbf3417756251d6990
Pranajli72 <pranjali100mi@gmail.com>
2025-11-03 11:59:40 +0530

    js file

---
+++ es6.js
@@ -0,0 +1,7 @@
+function fun(){
+    var a=20;
+    let name='xyz';
+
+    pi=3.14;
+    console.log("inside the function value")
+}
---
+++ function.js
@@ -0,0 +1,5 @@
+funA("xyz");
+function greet(name){
+    console.log('Hello,$(name)');
+greet("abc");
+}
